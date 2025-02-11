025ae9dd5be3905f9e685106841e9cc5d2fe0e32
powerdev93624 <michael1993624@gmail.com>
2025-02-11 11:16:01 +0900

    delete time

--- src/services/functions.py
+++ src/services/functions.py
@@ -15,10 +15,14 @@ doc = Document(original_doc_path)
 paragraphs = doc.paragraphs
 
 def replace_paragraph_text(paragraph, old_text, new_text):
+    print("A")
     for run in paragraph.runs:
+        print("B")
         # print(run.text)
         if old_text in run.text:
+            print("C")
             run.text = run.text.replace(old_text, new_text)
+            print("D")
 
 def replace_full_name(full_name):
     full_name_paragraph = paragraphs[full_name_paragraph_id]
@@ -122,19 +126,31 @@ def replace_recommendation(transcript, scores):
     for performance in performance_list:
         print(performance)
         if scores.loc[scores['id'] == performance, 'score'].values[0] >= 7:
+            print(0)
             strengthen_paragraph = paragraphs[globals()[f"{performance}_strengthen_paragraph_id"]]
+            print(1)
             replace_paragraph_text(strengthen_paragraph, performance, globals()[f"get_{performance}_strengthen"](transcript))
+            print(2)
             improve_paragraph = paragraphs[globals()[f"{performance}_improvement_paragraph_id"]]
+            print(3)
             p = improve_paragraph._element
+            print(4)
             p.getparent().remove(p)
-            time.sleep(10)
+            print(5)
+            # time.sleep(30)
         else:
+            print(0)
             improve_paragraph = paragraphs[globals()[f"{performance}_improvement_paragraph_id"]]
+            print(1)
             replace_paragraph_text(improve_paragraph, performance, globals()[f"get_{performance}_improve"](transcript))
+            print(2)
             strengthen_paragraph = paragraphs[globals()[f"{performance}_strengthen_paragraph_id"]]
+            print(3)
             p = strengthen_paragraph._element
+            print(4)
             p.getparent().remove(p)
-            time.sleep(10)
+            print(5)
+            # time.sleep(30)
 def replace_next_steps(next_steps):
     next_steps_paragraph = paragraphs[next_steps_paragraph_id]
     replace_paragraph_text(next_steps_paragraph, "Next steps", next_steps)
